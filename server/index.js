45f00f7f62559d28f7e03d944a4962b001c8c7f7
luchiiii <okwuosaoluchi95@gmail.com>
2026-07-17 18:51:37 +0100

    fix: restrict CORS to deployed frontend URL

--- server/index.js
+++ server/index.js
@@ -10,7 +10,11 @@ const chatRoutes = require("./routes/chat");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://expense-manager-three-tau.vercel.app/",
+  }),
+);
 app.use(express.json());
 
 app.use("/api/expenses", expenseRoutes);
